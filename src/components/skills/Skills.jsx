ce45dbbf45b107013ddcd524b3bca712eabc6850
Jeison Daza <derera199@gmail.com>
2022-02-13 12:41:59 -0400

    fix: fix media querys

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,17 +4,17 @@ import SubtitleParagraph from "~/components/SubtitleParagraph";
 import { skills } from "~/data/data.json";
 
 const Skills = () => {
-   return (
-      <section className="fullHeight container">
-         <h2 className="title">{skills.title}</h2>
-         <SubtitleParagraph content={skills.subtitle} />
-         <article className="tech_container">
-            {skills.content.map((el, index) => (
-               <SkillView content={el} key={index} />
-            ))}
-         </article>
-      </section>
-   );
+  return (
+    <section className="skills container">
+      <h2 className="title">{skills.title}</h2>
+      <SubtitleParagraph content={skills.subtitle} />
+      <article className="tech_container">
+        {skills.content.map((el, index) => (
+          <SkillView content={el} key={index} />
+        ))}
+      </article>
+    </section>
+  );
 };
 
 export default Skills;
