1301b30cf7b8edbc13cdec71824cd049f176e2d5
Jeison Daza <derera199@gmail.com>
2022-02-14 19:56:56 -0400

    feat: add  projects description

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,17 +1,34 @@
 import "./ProjectCard.css";
 
 const ProjectCard = ({ content, reverse }) => {
-   console.log(!reverse);
-   return (
-      <div className={`card ${!reverse && "reverse"}`}>
-         <img src={content.img} alt="project img" className="card_img" />
-         <div className="card_description">
-            <h3>{content.title}</h3>
-            <p>{content.description}</p>
-            <p className="card_btn">Learn more→</p>
-         </div>
+  console.log(!reverse);
+  return (
+    <div className={`card ${!reverse && "reverse"}`}>
+      <img src={content.img} alt="project img" className="card_img" />
+      <div className="card_description">
+        <h3>{content.title}</h3>
+        <p>{content.description}</p>
+        <div className="card_btns">
+          <a
+            href={content.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn_cta"
+          >
+            Go live
+          </a>
+          <a
+            href={content.repo}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn_text"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
 
 export default ProjectCard;
