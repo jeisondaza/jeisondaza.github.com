a333a189765dca9f9d4b0717dff1a840344a9c00
Jeison Daza <derera199@gmail.com>
2022-02-16 20:16:28 -0400

    refactor: media querys

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,12 +4,14 @@ import About from "./About";
 import { home } from "~/data/data.json";
 
 const Hero = () => {
-   return (
-      <section className="home_container">
-         <Avatar title={home.title} subtitle={home.subtitle} />
-         <About content={home.content} />
+  return (
+    <section className="home_container">
+      <section className="avatar_section">
+        <Avatar title={home.title} subtitle={home.subtitle} />
       </section>
-   );
+      <About content={home.content} />
+    </section>
+  );
 };
 
 export default Hero;
